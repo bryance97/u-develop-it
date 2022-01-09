7d5d030ec2980f98ca9b65bfd1587131b9dee10d
bryance97 <bryancifuentesescobar@gmail.com>
2022-01-08 21:05:54 -0500

    testing GET routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
